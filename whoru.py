f5812a3b664654d78dd0e42afb0ec3703c533081
zwy923 <811739729@qq.com>
2022-12-16 20:51:20 +0200

    12/16

--- whoru.py
+++ whoru.py
@@ -1,5 +1,158 @@
+import h5py
 import streamlit as st
-import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+import warnings
 
-st.title('Uber pickups in NYC')
+from sklearn.metrics import precision_recall_curve,accuracy_score,f1_score,precision_score,recall_score
+# suppress display of warnings
+warnings.filterwarnings('ignore')
+import os
+import cv2
+def load_image(path):
+    img = cv2.imread(path, 1)
+    # OpenCV loads images with color channels
+    # in BGR order. So we need to reverse them
+    return img[...,::-1]
+
+source_dir="./105_classes_pins_dataset"
+class IdentityMetadata():
+    def __init__(self, base, name, file):
+        self.base = base
+        # identity name
+        self.name = name
+        # image file name
+        self.file = file
+
+    def __repr__(self):
+        return self.image_path()
+
+    def image_path(self):
+        return os.path.join(self.base, self.name, self.file) 
+    
+def load_metadata(path):
+    metadata = []
+    for i in os.listdir(path):
+        for f in os.listdir(os.path.join(path, i)):
+            # Check file extension. Allow only jpg/jpeg' files.
+            ext = os.path.splitext(f)[1]
+            if ext == '.jpg' or ext == '.jpeg':
+                metadata.append(IdentityMetadata(path, i, f))
+    return np.array(metadata)
+
+# metadata = load_metadata('images')
+metadata = load_metadata(source_dir)
+
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import ZeroPadding2D, Convolution2D, MaxPooling2D, Dropout, Flatten, Activation
+
+def vgg_face():	
+    model = Sequential()
+    model.add(ZeroPadding2D((1,1),input_shape=(224,224, 3)))
+    model.add(Convolution2D(64, (3, 3), activation='relu'))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(64, (3, 3), activation='relu'))
+    model.add(MaxPooling2D((2,2), strides=(2,2)))
+    
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(128, (3, 3), activation='relu'))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(128, (3, 3), activation='relu'))
+    model.add(MaxPooling2D((2,2), strides=(2,2)))
+    
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(256, (3, 3), activation='relu'))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(256, (3, 3), activation='relu'))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(256, (3, 3), activation='relu'))
+    model.add(MaxPooling2D((2,2), strides=(2,2)))
+    
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(512, (3, 3), activation='relu'))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(512, (3, 3), activation='relu'))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(512, (3, 3), activation='relu'))
+    model.add(MaxPooling2D((2,2), strides=(2,2)))
+    
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(512, (3, 3), activation='relu'))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(512, (3, 3), activation='relu'))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(512, (3, 3), activation='relu'))
+    model.add(MaxPooling2D((2,2), strides=(2,2)))
+    
+    model.add(Convolution2D(4096, (7, 7), activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Convolution2D(4096, (1, 1), activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Convolution2D(2622, (1, 1)))
+    model.add(Flatten())
+    model.add(Activation('softmax'))
+    return model
+
+st.title('Smart System')
+st.text(metadata[20])
+model = vgg_face()
+model.load_weights('./vgg_face_weights.h5')
+
+
+model.layers[0], model.layers[-2]
+
+
+
+from tensorflow.keras.models import Model
+vgg_face_descriptor = Model(inputs=model.layers[0].input, outputs=model.layers[-2].output)
+
+type(vgg_face_descriptor)
+vgg_face_descriptor.inputs, vgg_face_descriptor.outputs
+
+
+# Get embedding vector for first image in the metadata using the pre-trained model
+img_path = metadata[20].image_path()
+img = load_image(img_path)
+
+# Normalising pixel values from [0-255] to [0-1]: scale RGB values to interval [0,1]
+img = (img / 255.).astype(np.float32)
+img = cv2.resize(img, dsize = (224,224))
+print(img.shape)
+
+# Obtain embedding vector for an image
+# Get the embedding vector for the above image using vgg_face_descriptor model and print the shape 
+embedding_vector = vgg_face_descriptor.predict(np.expand_dims(img, axis=0))[0]
+print(embedding_vector.shape)
+
+embedding_vector[0], type(embedding_vector), type(embedding_vector[0])
+embedding_vector[2], embedding_vector[98], embedding_vector[-2]
+total_images = len(metadata)
+print('total_images :', total_images)
+
+embeddings = np.zeros((metadata.shape[0], 2622))
+for i, m in enumerate(metadata):
+    img_path = metadata[i].image_path()
+    img = load_image(img_path)
+    img = (img / 255.).astype(np.float32)
+    img = cv2.resize(img, dsize = (224,224))
+    embedding_vector = vgg_face_descriptor.predict(np.expand_dims(img, axis=0))[0]
+    embeddings[i]=embedding_vector
+
+
+print('embeddings shape :', embeddings.shape)
+embeddings[0], embeddings[20], embeddings[20].shape
+embeddings[20]
+
+def distance(emb1, emb2):
+    return np.sum(np.square(emb1 - emb2))
+
+def show_pair(idx1, idx2):
+    plt.figure(figsize=(8,3))
+    plt.suptitle(f'Distance between {idx1} & {idx2}= {distance(embeddings[idx1], embeddings[idx2]):.2f}')
+    plt.subplot(121)
+    plt.imshow(load_image(metadata[idx1].image_path()))
+    plt.subplot(122)
+    plt.imshow(load_image(metadata[idx2].image_path()));    
+
+show_pair(0, 1)
+show_pair(0, 20)

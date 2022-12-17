ea491ff32d0e12462af62cf6e87be4211a7dfde6
zwy923 <811739729@qq.com>
2022-12-17 03:34:35 +0200

    Update whoru.py

--- whoru.py
+++ whoru.py
@@ -124,7 +124,7 @@ instructions = """
         """
 st.write(instructions)
 function_split=["Face recognition","Emotional recognition","Camera capture"]
-member=["Wenyue Zhang","Yufeng Pan","Jiaxuan Qi","Guowen Wang","Junyang Huang","Kecen Yin"]
+member=["Wenyue Zhang","Yufeng Pan","Jiaxuan Qi","Junyang Huang","Kecen Yin","Guowen Wang"]
 #start
 metadata = load_metadata(source_dir)
 model = vgg_face()
